fb15ebda019df52eeb5ee2af3cb31b7a05ac1a08
SuperSpiritEagle <superspiriteagle@gmail.com>
2022-12-16 11:57:25 +0500

    init

---
+++ IJExit/IJExit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IJExit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Для выхода из программы введите exit");
+
+            while (true)
+            {
+                string exit = Convert.ToString(Console.ReadLine());
+                if (exit == "exit")
+                {
+                    break;
+                }
+                Console.WriteLine(count++);
+            }
+        }
+    }
+}
